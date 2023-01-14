0c7d5db89e7ecc9fd9cbaaf7f26407563ecd5a5b
Alyssa Workman <starbirddancer@gmail.com>
2023-01-14 16:26:48 -0700

    corrected functions

--- node-calculator/index.js
+++ node-calculator/index.js
@@ -5,7 +5,7 @@ const num2 = parseInt(readLine.question('Enter second number:'));
 let operation = readLine.question('Enter operation: add, sub, mul, div:');
 
 if (operation === 'add'){
-console.log(`Result: ${add(num1, num2)}.`);
+    console.log(`Result: ${add(num1, num2)}.`);
 } else if (operation === 'mul'){
     console.log(`Result: ${mul(num1, num2)}.`);
 } else if (operation === 'div'){
@@ -22,7 +22,7 @@ function mul(){
     return num1 * num2;
 }
 
-function mul(){
+function div(){
     return num1 / num2;
 }
 
