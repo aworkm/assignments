fb3a96db9638d2f6ac4d2bf78508d26dee4b52d6
Alyssa Workman <starbirddancer@gmail.com>
2023-01-21 13:47:55 -0700

    array methods

---
+++ level1 /lessons/array-methods/index.js
@@ -0,0 +1,25 @@
+// JS Array Methods
+
+// Full list can be found here: https://www.w3schools.com/jsref/jsref_obj_array.asp
+
+var arrOfFood = ["pancake", "egg", "ice cream", "pizza", "hamburger"]
+
+// push - adds item(s) to the end of the array
+
+// pop - removes item from the end of the array
+
+// unshift - adds item(s) to the beginning of the array
+
+// shift - removes item from the beginning of the array
+
+// concat
+
+// indexOf
+
+// slice
+
+// join
+
+// reverse
+
+// splice
