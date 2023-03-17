2577f5afd8702e7a79b51f1659bc588378c40837
Alyssa Workman <starbirddancer@gmail.com>
2023-03-17 10:00:13 -0600

    bugs on a plane finish

--- level1 /assignment/bugs-on-a-plane/app.js
+++ level1 /assignment/bugs-on-a-plane/app.js
@@ -1,27 +1,33 @@
-var form = document.getElementByID("arline-form");
-var submit = document.getElementByID(submit);
-var query = document.querySelector;
+const form = document.getElementById("airline-form");
+const submit = document.getElementById("submit");
+//var query = document.querySelector;
 
-function formAlert{} {
-    var firstName = form.elements["firstName"].value;
-    var lastName = form.elements["lastName"].value;
-    var age = form.elements["age"].value;
-    var gender = form.elements["gender"].value;
-    var location = form.elements["travel-location"].value;
-    var diet = {};
-    if (form.elements['vegan'].checked) {
-        var diet.pop(document.getElementById("vegan").value);
+
+function formAlert() {
+
+    const firstName = form.elements["first-name"].value;
+    const lastName = form.elements["last-name"].value;
+    const age = form.elements["age"].value;
+    const gender = form.elements["gender"].value;
+    const location = form.elements["travel-location"].value;
+    const diet = [];
+
+    if (form.elements["vegan"].checked) {
+        diet.push(document.getElementById("vegan").value);
     }
-    if (form.elements['gluten'].checked) {
-        diet.push(document.getElementById('gluten').value);
+    if (form.elements["gluten"].checked) {
+        diet.push(document.getElementById("gluten").value);
     }
-    if (form.elements['paleo'].checked) {
-        diet.push(document.getElementById('paleo').value);
+    if (form.elements["paleo"].checked) {
+        diet.push(document.getElementById("paleo").value);
     }
 
 
-    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
-}
+    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet +  "\nAwesome, now if you die, it won't be an accident.."
+    );
+};
 
+submit.addEventListener("click", () => {
+    formAlert()
+});
 
-submit.addEventListener("click", formalert);
