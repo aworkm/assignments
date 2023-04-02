c4799c92f1857520e6d811c9aa6dfcbf78349b96
Alyssa Workman <starbirddancer@gmail.com>
2023-04-02 10:21:53 -0600

    colossal adventure rpg finish

--- level1 /projects/colossal-adventure-rpg/index.js
+++ level1 /projects/colossal-adventure-rpg/index.js
@@ -2,8 +2,8 @@
 //I need to prepare the variables first, that includes enableing readLine, a greeting message, a question for the userName, a changeable inventory (let), healthpoints (let) for user & possible temptations, 
 
 const readLine = require("readline-sync");
-const greeting = console.long("Greetings traveler, you are about to embark on a journey of a lifetime, to collect knowledge from the greatest Guru known. It will take stamina, patience, discernment from temptation and willingness to learn...");
-const userName = readline.question("Before we bein, please tell me your name: ");
+const greeting = console.log("Greetings traveler, you are about to embark on a journey of a lifetime to collect knowledge by resistance to tempations like Doubt, Negative self-talk, Procrastination, and Distractions. It will take stamina, patience, discernment from temptation and willingness to learn in order to keep moral high.");
+const userName = readLine.question("Before we bein, please tell me your name: ");
 let userInventory = [];
 let hasItems = false;
 let hp = 100;
@@ -16,18 +16,18 @@ let experience = 0;
 
 function walk() {
     const options = ["Walk", "Traveler Info", "Equip Resistance to Temptation"];
-    const userOption = readline.keyInSelect( options, "Make a choice from the following options:"
+    const userOption = readLine.keyInSelect( options, "Make a choice from the following options:"
     );
     if (userOption === 0) {
         const temptationAppears = Math.random();
         if (temptationAppears <= 0.4) {
             fight();
         }
-    } else if (userOptino === 1) {
-    console.log (`\nUsername: ${userName}\nHP: ${hp}\nItems: ${userInventory}\nLevel: ${playerLevel1}\n`);
+    } else if (userOption === 1) {
+    console.log (`\nUsername: ${userName}\nHP: ${hp}\nItems: ${userInventory}\nLevel: ${playerLevel}\n`);
 //If user has items and user will run if === true;
     } else if (userOption === 2 && hasItems) {
-        const equipResistance = readLine.keyInYN("Do you want to resist this temptation to increase your chances to reach enlightenment?");
+        const equipResistance = readLine.keyInYN("Do you want to equipt mindset tools to resist this temptation and increase your chances to reach enlightenment?");
         if (equipResistance === true) {
                 resistanceEquipped = true;
         }
@@ -39,29 +39,29 @@ function walk() {
 // Fight or Run fucntion//
 function fight() {
     const willFight = readLine
-        .keyIn("A temptation has appeared! Press 'f' to fight or 'r' to resist and run.", { limit: "fr" }
+        .keyIn("A temptation has appeared! Press 'f' to fight temptation or 'r' to resist and run.", { limit: "fr" }
         )
         .toLowerCase();
-    const atttemptToRun = Math.random();
+    const attemptToRun = Math.random();
     if (willFight === "f") {
         determineWinner();
     } else if (willFight === "r" && attemptToRun >= 0.5) {
-        console.log("You have escaped temptation, one step closer to enlightenment.");
+        console.log("You have escaped temptation, one step closer to enlightenment!");
     } else if (attemptToRun < 0.5) {
-        console.log("Your attempt to leave the temptation has faltered, you must prepare to lose some enlightnement.");
+        console.log("Your attempt to leave the temptation has failed, you must prepare to lose some moral.");
         determineWinner();
     }
 }
 // Function for type of temptation and what item user will use to resist the temptation. 
 function determineWinner() {
-    const tempatations = ["Doubt", "Procrastination", "Distraction", "Negative Self-Talk"];
-    const randomTemptation = Math.floor(Math.random() * enemies.length);
+    const temptations = ["Doubt", "Procrastination", "Distraction", "Negative Self-Talk"];
+    const randomTemptation = Math.floor(Math.random() * temptations.length);
     const temptation = temptations[randomTemptation];
-    const items = ["Affirmation", "Calling guides and guardians for strenth", "Mediation", "Banishment"];
+    const items = ["Affirmation", "Calling Guides and Guardians", "Mediate", "Banish It"];
     const randomItems = Math.floor(Math.random() * items.length);
     const playerItem = items [randomItems];
 
-    while (hp > 0 && enemyHp > 0) {
+    while (hp > 0 && temptationHp > 0) {
         const playerDmg = randomDmg(10, 16);
         const temptationDmg = randomDmg(5, 8);
         const bonusDMG = randomDmg(5,11);
@@ -71,7 +71,7 @@ function determineWinner() {
             console.log(
                 `\nYou abstain from the ${temptation} for ${playerDmg} points of moral loss + ${bonusDMG} points of additional moral loss!\n`
             );
-            temptaionHp = temptationHp - (playerDmg + bonusDMG);
+            temptationHp = temptationHp - (playerDmg + bonusDMG);
         } else {
             console.log(`\nYou abstain from the ${temptation} for ${playerDmg} points of abstinence!`);
             temptationHp = temptationHp - playerDmg;
@@ -79,9 +79,9 @@ function determineWinner() {
         console.log(`\nThe ${temptation} lurs you in for ${temptationDmg} points of moral loss!\n`);
         hp = hp - temptationDmg;
         if (temptationHp <= 0) {
-            console.log(`\nYou have overcome the temptation and moral has been restored to you and you have been equipped with more tools in your mindset. You have gained closer proximity to enlightenment!\n`
+            console.log(`\nYou have overcome the temptation and moral has been restored to you. You have been boosted with more tools in your mindset to over come temptation. You have gained closer proximity to enlightenment!\n`
             );
-            experince = experince + 30;
+            experience = experience + 30;
             if (experience === 100) {
                 playerLevel++;
                 hp = 100;
@@ -109,7 +109,7 @@ function determineWinner() {
 }
 // Function to allow player to play again or not.
 function playAgain() {
-    const playAgain = readLine.keyInYN("Would you like to journey again?");
+    const playAgain = readLine.keyInYN("Would you like to journey again? ");
     if (playAgain === true) {
         hp = 100;
     } else {
