399b963a3c2b0929ca0867a3d250bd7205c2c6f7
Alyssa Workman <starbirddancer@gmail.com>
2023-04-03 06:57:28 -0600

    final touches

--- level1 /projects/colossal-adventure-rpg/index.js
+++ level1 /projects/colossal-adventure-rpg/index.js
@@ -1,8 +1,8 @@
 "use strict";
-//I need to prepare the variables first, that includes enableing readLine, a greeting message, a question for the userName, a changeable inventory (let), healthpoints (let) for user & possible temptations, 
+//I need to prepare the variables first, that includes enableing readLine to run in node, a greeting message, a question for the userName, usable mindeset tools(let), healthpoints (let) for user & temptations, the user can level up if they gain experince.  
 
 const readLine = require("readline-sync");
-const greeting = console.log("Greetings traveler, you are about to embark on a journey of a lifetime to collect knowledge by resistance to temptations like Doubt, Negative self-talk, Procrastination, and Distractions. It will take stamina, patience, and discernment in order to keep moral high.");
+const greeting = console.log("Greetings traveler, you are about to embark on a journey of a lifetime to collect knowledge by resistance to temptations like doubt, negative self-talk, procrastination, and distraction. It will take stamina, patience, and discernment in order to keep moral high.");
 const userName = readLine.question("Before we begin, please tell me your name: ");
 let userTools = [];
 let hasTools = false;
@@ -12,64 +12,67 @@ let resistanceEquipped = false;
 let playerLevel = 1; 
 let experience = 0;
 
-//with all variables defined now I need to write the walk function, game options, user option to key in their choice selection, then use if statments that run through the options with random chance of temptation appearing. 
+//with all variables defined now I need to write the walk function, game options, user option to key in their choice selection, then use if statments that run through the options with random chances of temptation appearing. 
 
 function walk() {
+    //opted for options vs plyer array, so the player has choices what to do. If i did the player array the user would be able to input alphabet options, this way keeps the keys conscise in one place like a console game with option 1, 2, 3, or 0 to exit.
     const options = ["Walk", "Traveler Info", "Equip Resistance to Temptation"];
+    //if else statements to go through the options 
     const userOption = readLine.keyInSelect( options, "Make a choice from the following options:"
     );
+    //if statement for chance encounter with temptation
     if (userOption === 0) {
         const temptationAppears = Math.random();
         if (temptationAppears <= 0.4) {
             fight();
         }
+// option to print user info (user name, tools, helth points, & current level)
     } else if (userOption === 1) {
-    console.log (`\nUsername: ${userName}\nHP: ${hp}\nTools: ${userTools}\nLevel: ${playerLevel}\n`);
-//If user has tools and user will run if === true;
+    console.log (`\nUsername: ${userName}\nHP: ${hp}\nMindsetTools: ${userTools}\nLevel: ${playerLevel}\n`);
+//If user has tools to equip === true; otherwise the the game will continue. 
     } else if (userOption === 2 && hasTools) {
         const equipResistance = readLine.keyInYN("Do you want to equipt mindset tools to resist temptation and increase your chances to reach enlightenment?");
         if (equipResistance === true) {
                 resistanceEquipped = true;
         }
+//option to exit the game
     } else if (userOption === -1) {
         hp = 0;
         playAgain();
     }
 } 
-// Fight or Run fucntion and variables with 50% chance of happening//
+// Fight or Run fucntion and variables with 50% chance of escaping//
 function fight() {
-    const willFight = readLine
-        .keyIn("A temptation has appeared! Press 'f' to fight temptation or 'r' to resist and run.", { limit: "fr" }
-        )
+    const willFight = readLine.keyIn("A temptation has appeared! Press 'f' to fight temptation or 'r' to resist and run.", { limit: "fr" })
         .toLowerCase();
     const attemptToRun = Math.random();
     if (willFight === "f") {
-        determineWinner();
+        determineInfluence();
     } else if (willFight === "r" && attemptToRun >= 0.5) {
         console.log("You have escaped temptation, one step closer to enlightenment!");
     } else if (attemptToRun < 0.5) {
         console.log("Your attempt to leave the temptation has failed, you have lost some moral.");
-        determineWinner();
+        determineInfluence();
     }
 }
-// Function for type of temptation and what item user will use to resist the temptation. 
-function determineWinner() {
-    const temptations = ["Doubt", "Procrastination", "Distraction", "Negative Self-Talk"];
+// Function for type of temptation influence and what mindset tool user will use to resist the temptation. 
+function determineInfluence() {
+    const temptations = ["Doubt", "Procrastination", "Distraction", "Negative Self-Talk", "Fear"];
     const randomTemptation = Math.floor(Math.random() * temptations.length);
     const temptation = temptations[randomTemptation];
-    const tools = ["Affirmation", "Guides and Guardians", "Meditate", "Change Mindset"];
+    const tools = ["Affirmation", "Guides & Guardians", "Meditate", "Change Mindset", "Courage"];
     const randomTools = Math.floor(Math.random() * tools.length);
     const playerTool = tools [randomTools];
-
+//While loop to generate the random damage that can happen and the attack key option.
     while (hp > 0 && temptationHp > 0) {
         const playerDmg = randomDmg(10, 16);
         const temptationDmg = randomDmg(5, 8);
         const bonusDMG = randomDmg(5,11);
         const attack = readLine.keyIn("Press 'a' to abstain from temptation! ", { limit: "a"});
-//Check for tools equipped to abstain from temptation
+//Check for tools equipped to abstain from temptation and possibile results if the temptation is successful in a counter attack
         if (resistanceEquipped === true){
             console.log(
-                `\nYou abstain from the ${temptation} for ${playerDmg} points + ${bonusDMG} of additional moral loss for even looking!\n`
+                `\nYou abstain from the ${temptation} for ${playerDmg} points + ${bonusDMG} for fighting the urge to give in!\n`
             );
             temptationHp = temptationHp - (playerDmg + bonusDMG);
         } else {
@@ -78,6 +81,7 @@ function determineWinner() {
         }
         console.log(`\nThe ${temptation} lurs you in for ${temptationDmg} points of moral loss!\n`);
         hp = hp - temptationDmg;
+//if player is successful in escaping with minimal damage, then player gets points to heal and/or level up, 
         if (temptationHp <= 0) {
             console.log(`\nYou have overcome the temptation and moral has been restored to you by 40 points! You now have more tools in your mindset to over come temptation. You are getting closer to enlightenment!\n`
             );
@@ -90,7 +94,7 @@ function determineWinner() {
                 );
             }
             hasTools = true;
-//Add if statment that only adds up to 100 hp, 
+//Add if statment that only adds up to 100 hp before leveloing up, 
             if (hp < 100) {
                 hp = hp + 30;
             }
@@ -107,13 +111,13 @@ function determineWinner() {
         }
     }
 }
-// Function to allow player to play again or not.
+// Function to allow player to play again or not. otherwise will terminate the game.
 function playAgain() {
     const playAgain = readLine.keyInYN("Would you like to try again? ");
     if (playAgain === true) {
         hp = 100;
     } else {
-        console.log("Come back when you are ready to overcome temptation to reach enlightenment!");
+        console.log("Come back when you are ready to overcome temptation and reach enlightenment!");
     }
 }
 // Random damage range when temptations are encountered
--- level1 /projects/colossal-adventure-rpg/notes.md
+++ level1 /projects/colossal-adventure-rpg/notes.md
@@ -5,23 +5,23 @@ The point of this exercise is to better master Javascript functions, loops, cond
 
 **Project Requirements:** 
 
-1. Console must greet player with a fun message -- "Greetings traveler, you are about to embark on a journey of a lifetime, to collect knowledge from the greatest gurus around the world. It will take stamina, patience, and willingness to learn, but first...tell me your name?"
-2. Console must ask for the player's name and store it -- "Pleasure to know your name ___ , although I am sure you will recieve many more along your way. Are you ready to begin?"
+1. Console must greet player with a fun message -- "Greetings traveler, you are about to embark on a journey of a lifetime, to collect knowledge by adverting from temptations.. It will take stamina, patience, and willingness to learn, but first...tell me your name?"
+2. Console must ask for the player's name and store it -- "Pleasure to know your name ___ " " Are you ready to begin?"
 3. Walking:
 - The console will ask the user to enter a "w" to walk
-- Every time the player walks, a random algorithm will be run that determines if a Guru has appeared (A 1/3 or 1/4 chance of encounter)
+- Every time the player walks, a random algorithm will be run that determines if a temptation has appeared (A 1/3 or 1/4 chance of encounter)
 - Use a while loop to control this flow.
-1. If a guru appears:
-- The guru is random (can be chosen out of a minimum of 3 different Guru names)
-- The user can decide to learn or return home
-- If learning, a random amount of wisdom will be dealt between a min and max
+1. If a temptation appears:
+- The temptation is random (can be chosen out of a minimum of 3 different temptation names)
+- The user can decide to fight or return home
+- If fighting, a random amount of moral will be dealt between a min and max
 - If retruning, there will be a 50% chance of losing all knowledge
-- After the player learns or returns home the Guru sends chi back for a random hp amount
-- The player and Guru will exchange energy each other in a loop until one of them passes out or deems success.
-- If the player learns all from the guru you can give the Player some HP and special item that is stored in the inventory . After this, the player will continue walking.
-- If the Guru deems the player not worthy the console prints a cool assuring message and the game ends -- "Sorry, you cannot continue this journey until the Guru's deem fit to bestow all wsisom upon you. Please return home and try again for the journy to enlightenment."
+- After the player resists or returns fights the temptation strikes back for a random hp amount
+- The player and temptation will exchange energy each other in a loop until one of them gives in.
+- If the player resists and fight the Player will recieve some HP and special item that is stored in the inventory . After this, the player will continue walking.
+- If the player gives into temptation the console prints a cool assuring message and the game ends -- "Come back when you are ready to face temptation to reach enlightment."
 1. Inventory
-- When the player is granted the Guru blessing, they are awarded with enlightenment (optional)
+- When the player is successfl in resisting temptation, they are awarded with a level up (optional)
 - If the user enters 'Print' or 'p' in the console, the console will print the players name, HP, and each item in their inventory
 
-The story is the warroirs journey to enlightnment. Temptation along the way that takes vitality, If the player is successful to meet the Guru and earn the wisdom of the greatest teacher, then they will reach enlightenment. If giving into temptation and distraction, the player will need to return home for a nap.
+The story is the warroirs journey to enlightnment. Temptation along the way that takes vitality and moral. If successful the player will reach a new level closer to enlightenment. If giving into temptation and distraction, the player will need to return to try again..
